b0eafe920711e03e228db9ccb36d1eca3270be2b
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2024-06-04 23:08:01 +0600

    Folder style updated

---
+++ Assignment-1/findMinimumValue.php
@@ -0,0 +1,23 @@
+<?php
+
+$testList = [10, -12, 34, 12, -3, 123];
+
+function findMinValue($numberList)
+{
+
+  $listLength = count($numberList);
+
+  $min = $numberList[0];
+
+  for ($i = 1; $i < $listLength; $i++) {
+
+    if (abs($min) > abs($numberList[$i])) {
+
+      $min = abs($numberList[$i]);
+
+    }
+  }
+  return $min;
+}
+
+echo findMinValue($numberList);
