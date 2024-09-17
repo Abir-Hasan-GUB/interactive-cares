7b6007363c17c0304679ba877465b54d977e3f75
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2024-09-18 00:23:43 +0600

    Assignment 7 start

---
+++ Assignment-7/barta/resources/views/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="{{ asset('https://cdn.tailwindcss.com') }}"></script>
+
+    <!-- AlpineJS CDN -->
+    <script defer src="{{ asset('https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js') }}"></script>
+
+    <link rel="preconnect" href="{{ asset('https://fonts.googleapis.com') }}" />
+    <link rel="preconnect" href="{{ asset('https://fonts.gstatic.com') }}" crossorigin />
+    <link href="{{ asset('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap') }}"
+        rel="stylesheet" />
+
+    <style>
+        * {
+            font-family: 'Inter', sans-serif;
+        }
+    </style>
+</head>
+
+<body class="bg-gray-100">
+
+    @if (Route::currentRouteName() !== 'login' && Route::currentRouteName() !== 'register')
+        @include('componants.header.nav')
+    @endif
+
+    <section id="content-div">
+        <main
+            class="{{ Route::currentRouteName() !== 'login' && Route::currentRouteName() !== 'register' ? 'container max-w-xl mx-auto space-y-8 mt-8 px-2 md:px-0 min-h-screen' : '' }}">
+            @yield('content')
+        </main>
+    </section>
+
+    @if (Route::currentRouteName() !== 'login' && Route::currentRouteName() !== 'register')
+        @include('componants.footer.footer')
+    @endif
+
+</body>
+
+</html>
---
+++ Assignment-7/barta/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.index');
+})->name('home');
+
+// login
+Route::get('/login', [UserController::class, 'index'])->name('login');
+Route::get('/register', [UserController::class, 'create'])->name('register');
+
